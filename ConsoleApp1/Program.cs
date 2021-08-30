2e56357c8b45bc0b2160a0c1296fbaee5be3e4e3
DIEGO <DIEGO@DESKTOP-VLF2ESU>
2021-08-30 12:41:05 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //sumar
+            int a = 4;
+            int b = 2;
+            Console.WriteLine(a + b);
+            Console.ReadKey();
+        }
+    }
+}
